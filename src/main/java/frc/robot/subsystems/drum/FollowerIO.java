e1f9b869e7f34f81114488e6e0feb413b9916e48
SCool62 <samthecool300@icloud.com>
2026-09-14 12:14:26 -0700

    FollowerIO

---
+++ src/main/java/frc/robot/subsystems/drum/FollowerIO.java
@@ -0,0 +1,89 @@
+package frc.robot.subsystems.drum;
+
+import static edu.wpi.first.units.Units.Celsius;
+import static edu.wpi.first.units.Units.Rotation;
+import static edu.wpi.first.units.Units.Second;
+
+import org.littletonrobotics.junction.AutoLog;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
+
+public class FollowerIO {
+    @AutoLog
+    public static class FollowerIOInputs {
+        public double velocityRotPerSec = 0.0;
+        public double voltage = 0.0;
+        public double statorCurrentAmps = 0.0;
+        public double supplyCurrentAmps = 0.0;
+        public double tempC = 0.0;
+        // For sysid
+        public double flywheelPositionRotations = 0.0;
+    }
+
+    public final TalonFX motor;
+
+    private StatusSignal<AngularVelocity> velocity;
+    private StatusSignal<Voltage> voltage;
+    private StatusSignal<Current> statorCurrent;
+    private StatusSignal<Current> supplyCurrent;
+    private StatusSignal<Temperature> temp;
+    private StatusSignal<Angle> flywheelPosition;
+
+    private Follower followerReq;
+
+    public FollowerIO(int motorID, int leaderID, MotorAlignmentValue alignment, CANBus canBus) {
+        // TODO: CORRECT ID
+        motor = new TalonFX(motorID, canBus);
+
+        velocity = motor.getVelocity();
+        voltage = motor.getMotorVoltage();
+        statorCurrent = motor.getStatorCurrent();
+        supplyCurrent  = motor.getSupplyCurrent();
+        temp  = motor.getDeviceTemp();
+        flywheelPosition  = motor.getPosition();
+
+        BaseStatusSignal.setUpdateFrequencyForAll(
+            50.0, 
+            velocity,
+            voltage,
+            statorCurrent,
+            supplyCurrent,
+            temp,
+            flywheelPosition
+        );
+        motor.optimizeBusUtilization();
+
+        followerReq = new Follower(leaderID, alignment);
+        motor.setControl(followerReq);
+    }
+
+    public void updateInputs(FollowerIOInputs inputs) {
+        BaseStatusSignal.refreshAll(
+            velocity,
+            voltage,
+            statorCurrent,
+            supplyCurrent,
+            temp,
+            flywheelPosition
+        );
+        inputs.flywheelPositionRotations = flywheelPosition.getValue().in(Rotation);
+        inputs.velocityRotPerSec = velocity.getValue().in(Rotation.per(Second));
+        inputs.voltage = voltage.getValueAsDouble();
+        inputs.statorCurrentAmps = statorCurrent.getValueAsDouble();
+        inputs.supplyCurrentAmps = supplyCurrent.getValueAsDouble();
+        inputs.tempC = temp.getValue().in(Celsius);
+    }
+}
