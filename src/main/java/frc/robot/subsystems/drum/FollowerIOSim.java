bcc61281e9b5b265e9cf0ec7a27465404fa4d2dd
SCool62 <samthecool300@icloud.com>
2026-09-15 14:17:56 -0700

    Follower IO Sim (not sure whether this even works)

---
+++ src/main/java/frc/robot/subsystems/drum/FollowerIOSim.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems.drum;
+
+import java.util.function.DoubleSupplier;
+
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import com.ctre.phoenix6.sim.TalonFXSimState;
+
+public class FollowerIOSim extends FollowerIO {
+    private TalonFXSimState simState;
+
+    private final DoubleSupplier leaderPositionSupplier;
+    private final DoubleSupplier leaderVelSupplier;
+
+    public FollowerIOSim(int motorID, int leaderID, MotorAlignmentValue alignment, CANBus canBus, DoubleSupplier leaderPositionSupplier, DoubleSupplier leaderVelSupplier) {
+        super(motorID, leaderID, alignment, canBus);
+
+        // TODO: I'M NOT EVEN SURE THIS WORKS
+        this.leaderPositionSupplier = leaderPositionSupplier;
+        this.leaderVelSupplier = leaderVelSupplier;
+    }
+
+    @Override
+    public void updateInputs(FollowerIOInputs inputs) {
+        // First update sim state with the mech pos and vel
+        simState.setRawRotorPosition(leaderPositionSupplier.getAsDouble());
+        simState.setRotorVelocity(leaderVelSupplier.getAsDouble());
+
+        super.updateInputs(inputs);
+    }
+}
